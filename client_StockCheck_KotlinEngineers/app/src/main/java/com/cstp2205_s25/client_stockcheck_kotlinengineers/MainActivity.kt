4f7b9dea060aceee6e13ecf0a3559594a7da76e0
Amanpreet Kaur <000466810@student.vcc.ca>
2025-05-29 17:17:30 -0700

    chore: initialize repo with frontend and backend directories

---
+++ client_StockCheck_KotlinEngineers/app/src/main/java/com/cstp2205_s25/client_stockcheck_kotlinengineers/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cstp2205_s25.client_stockcheck_kotlinengineers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cstp2205_s25.client_stockcheck_kotlinengineers.ui.theme.Client_StockCheck_KotlinEngineersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Client_StockCheck_KotlinEngineersTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Client_StockCheck_KotlinEngineersTheme {
+        Greeting("Android")
+    }
+}
