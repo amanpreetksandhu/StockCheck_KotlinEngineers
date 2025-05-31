b026855c165924e18dd6d104e6bc6e89a2244610
Amanpreet Kaur <000466810@student.vcc.ca>
2025-05-30 23:25:44 -0700

    feat: create SignupScreen UI and reusable components for input and button

---
+++ client_StockCheck_KotlinEngineers/app/src/main/java/com/cstp2205_s25/client_stockcheck_kotlinengineers/components/ComposeTextField.kt
@@ -0,0 +1,24 @@
+package com.cstp2205_s25.client_stockcheck_kotlinengineers.components
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.VisualTransformation
+
+@Composable
+fun ComposeTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        modifier = modifier,
+        visualTransformation = visualTransformation
+    )
+}
