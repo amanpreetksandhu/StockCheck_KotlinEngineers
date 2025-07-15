fdc880b143ce15a55831e8d0788e488682f1e1a0
Amanpreet Kaur <000466810@student.vcc.ca>
2025-07-14 19:37:53 -0700

    fix

--- server_StockCheck_KotlinEngineers/routes/inventory.js
+++ server_StockCheck_KotlinEngineers/routes/inventory.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const inventoryController = require('../controllers/inventoryController');
+const inventoryController = require('../controllers/InventoryController');
 
 // Routes
 router.get('/', inventoryController.getAllItems);
