b809656961661a02cd68833b0834185008309d80
Amanpreet Kaur <000466810@student.vcc.ca>
2025-07-14 19:45:21 -0700

    chore: route import fix inside inventoryController

--- server_StockCheck_KotlinEngineers/routes/inventory.js
+++ server_StockCheck_KotlinEngineers/routes/inventory.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const inventoryController = require('../controllers/InventoryController');
+const inventoryController = require('../controllers/inventoryController');
 
 // Routes
 router.get('/', inventoryController.getAllItems);
